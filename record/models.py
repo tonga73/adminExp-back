c2bb5a6e09a80c972a67eb06f9c72a017888cadd
gastoire <buzzcutseason73@gmail.com>
2021-04-21 06:10:30 -0300

    Agregados campos nuevos

--- record/models.py
+++ record/models.py
@@ -7,4 +7,7 @@ class Record(models.Model):
   expertise_status = models.CharField(max_length=50, default='python')
   payment_status = models.CharField(max_length=50, default='python')
   arrangement_type = models.CharField(max_length=50, default='python')
-  updated_state = models.CharField(max_length=50, default='python')
+  action_lawyer = models.CharField(max_length=50, default='python') 
+  comments = models.CharField(max_length=550, default='python') 
+  dependency = models.CharField(max_length=50, default='python') 
+  city = models.CharField(max_length=50, default='python') 
